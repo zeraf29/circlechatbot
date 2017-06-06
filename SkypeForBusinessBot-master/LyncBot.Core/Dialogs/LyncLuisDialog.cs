f5e1b6fa0d584b9d205968ef15d9f430f12c70b6
zeraf29 <zeraf29@gmail.com>
2017-06-06 17:51:04 +0900

    소스 재 커밋

---
+++ SkypeForBusinessBot-master/LyncBot.Core/Dialogs/LyncLuisDialog.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LyncBot.Core.Dialogs
+{
+    [LuisModel("90ff3c2e-6afe-41b0-ae10-21b4000abc65", "249e15ad49a44eb7816c5b32bfe8b3ca")]
+    [Serializable]
+    public class LyncLuisDialog : LuisDialog<object>
+    {
+        private PresenceService _presenceService;
+
+        public LyncLuisDialog(PresenceService presenceService)
+        {
+            _presenceService = presenceService;
+        }
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            //await context.PostAsync("I'm sorry. I didn't understand you.");
+            // Dont do anything. Pretend I am busy.
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("HiGreetings")]
+        public async Task HiGreetings(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            var activity = await message;
+            string name = GetName(activity.From);
+            await context.PostOnlyOnceAsync(Responses.HiGreetingsResponse(name), nameof(HiGreetings));
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("GoodMorningGreetings")]
+        public async Task GoodMorningGreetings(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            string name = string.Empty;
+            if (!context.PrivateConversationData.ContainsKey(nameof(HiGreetings)))
+            {
+                var activity = await message;
+                name = GetName(activity.From);
+            }
+            await context.PostOnlyOnceAsync(Responses.GoodMorningGreetingsResponse(name), nameof(GoodMorningGreetings));
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("Call")]
+        public async Task Call(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            await context.PostOnlyOnceAsync(Responses.CallResponse(), nameof(Call));
+            _presenceService.SetPresenceBusy();
+            context.Wait(MessageReceived);
+        }
+
+        [LuisIntent("날씨")]
+        public async Task Weather(IDialogContext context, IAwaitable<IMessageActivity> message, LuisResult result)
+        {
+            string zoneCode = string.Empty;
+            foreach (EntityRecommendation e in result.Entities)
+            {
+                if (e.Entity.Contains("서 울"))
+                    zoneCode = "1100000000";
+                else if (e.Entity.Contains("부 산"))
+                    zoneCode = "2600000000";
+                else if (e.Entity.Contains("대 구"))
+                    zoneCode = "2700000000";
+                else if (e.Entity.Contains("인 천"))
+                    zoneCode = "2800000000";
+                else if (e.Entity.Contains("광 주"))
+                    zoneCode = "2900000000";
+                else if (e.Entity.Contains("대 전"))
+                    zoneCode = "3000000000";
+                else if (e.Entity.Contains("울 산"))
+                    zoneCode = "3100000000";
+                else if (e.Entity.Contains("세 종"))
+                    zoneCode = "3611000000";
+                else if (e.Entity.Contains("경 기"))
+                    zoneCode = "4100000000";
+                else if (e.Entity.Contains("강 원"))
+                    zoneCode = "4200000000";
+                else if (e.Entity.Contains("충 남"))
+                    zoneCode = "4400000000";
+                else if (e.Entity.Contains("전 북"))
+                    zoneCode = "4500000000";
+                else if (e.Entity.Contains("전 남"))
+                    zoneCode = "4600000000";
+                else if (e.Entity.Contains("경 북"))
+                    zoneCode = "4700000000";
+                else if (e.Entity.Contains("경 남"))
+                    zoneCode = "4800000000";
+                else if (e.Entity.Contains("제 주"))
+                    zoneCode = "5000000000";
+
+                await context.PostAsync(Responses.WeatherResponse(zoneCode), nameof(Weather));
+
+                context.Wait(MessageReceived);
+            }
+        }
+
+        private static string GetName(ChannelAccount from)
+        {
+            string name = string.Empty;
+            if (string.IsNullOrEmpty(from.Name))
+                return name;
+
+            var res = from.Name.Split(' ');
+            foreach (var item in res)
+            {
+                if (item.Length > 1)
+                {
+                    name = item;
+                    break;
+                }
+            }
+            return name;
+        }
+    }
+}
---
+++ SkypeForBusinessBot-master/LyncBot.Core/Responses.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Xml;
+using System.Net.Cache;
+
+namespace LyncBot.Core
+{
+    public static class Responses
+    {
+        public static List<string> HiGreetingsResponse(string name)
+        {
+            var greetings = new List<string> { "hi", "hello", "hey" };
+            return AppendName(greetings, name);
+        }
+        public static List<string> GoodMorningGreetingsResponse(string name)
+        {
+            var greetings = GetGreeting();
+            return AppendName(greetings, name);
+        }
+
+        public static List<string> CallResponse()
+        {
+            return new List<string>
+            {
+                "I am little busy right now. Can we talk after an hour?",
+                "In a meeting",
+                "Busy now. Can we talk later?"
+            };
+        }
+
+        private static List<string> GetGreeting()
+        {
+            var afternoon = 12;
+            var evening = 16;
+            if (DateTime.Now.Hour < afternoon)
+                return new List<string> { "Good Morning", "gm", "vgm" };
+            else if (DateTime.Now.Hour < evening)
+                return new List<string> { "Good Afternoon" };
+            else
+                return new List<string> { "Good Evening" };
+        }
+
+        public static List<string> WeatherResponse(string zoneCode)
+        {
+            if (string.IsNullOrEmpty(zoneCode))
+            {
+                return new List<string>
+                {
+                    "지역을 입력해주세요."
+                };
+            }
+            string result = string.Empty;
+            string temp = string.Empty;
+            string wfKor = string.Empty;
+            string pop = string.Empty;
+            string wdKor = string.Empty;
+            string ws = string.Empty;
+            string reh = string.Empty;
+
+            string reqUrl = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=" + zoneCode;
+
+            HttpWebRequest HttpWRequest = (HttpWebRequest)WebRequest.Create(reqUrl);
+            HttpWRequest.Proxy = null;
+            HttpWRequest.PreAuthenticate = true;
+            HttpWRequest.ServicePoint.ConnectionLimit = 32;
+            HttpWRequest.Pipelined = true; //
+            HttpWRequest.KeepAlive = true; //this is the default
+
+            HttpWRequest.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.NoCacheNoStore);
+            HttpWRequest.Headers["Cache-Control"] = "no-cache";
+            HttpWRequest.Headers["Pragma"] = "no-cache";
+
+            HttpWRequest.Timeout = 6000;
+            HttpWRequest.ContentType = "text/xml";
+
+            System.IO.Stream responseStream = HttpWRequest.GetResponse().GetResponseStream();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(responseStream);
+
+            XmlNode rootNode = doc.SelectSingleNode("/rss");
+            if (rootNode != null)
+            {
+                XmlNode channelNode = rootNode.SelectSingleNode("channel");
+                if (channelNode != null)
+                {
+                    XmlNode itemNode = channelNode.SelectSingleNode("item");
+                    if (itemNode != null)
+                    {
+                        XmlNode descriptionNode = itemNode.SelectSingleNode("description");
+                        if (descriptionNode != null)
+                        {
+                            XmlNode bodyNode = descriptionNode.SelectSingleNode("body");
+                            if (bodyNode != null)
+                            {
+                                XmlNodeList dataList = bodyNode.SelectNodes("data");
+                                foreach (XmlNode dataNode in dataList)
+                                {
+                                    if (dataNode.Attributes.GetNamedItem("seq").InnerText == "0")
+                                    {
+                                        XmlNodeList nodeList = dataNode.ChildNodes;
+                                        {
+                                            if (nodeList != null)
+                                            {
+                                                foreach (XmlNode node in nodeList)
+                                                {
+                                                    if (string.Equals(node.Name, "temp")) //현재 온도
+                                                    {
+                                                        temp = node.InnerText;
+                                                    }
+                                                    if (string.Equals(node.Name, "wfKor")) //구름
+                                                    {
+                                                        wfKor = node.InnerText;
+                                                    }
+                                                    if (string.Equals(node.Name, "pop")) //강수확률
+                                                    {
+                                                        pop = node.InnerText;
+                                                    }
+                                                    if (string.Equals(node.Name, "wdKor")) //풍향
+                                                    {
+                                                        wdKor = node.InnerText;
+                                                    }
+                                                    if (string.Equals(node.Name, "ws")) //풍속
+                                                    {
+                                                        ws = node.InnerText;
+                                                    }
+                                                    if (string.Equals(node.Name, "reh")) //습도
+                                                    {
+                                                        reh = node.InnerText;
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            result = "현재 온도 " + temp + "˚C며, 하늘은 " + wfKor + "이며 강수 확률은 " + pop + "%입니다. " + wdKor + "풍이 " + ws + "m/s로 불고 있으며, 습도는 " + reh + "%입니다.";
+
+            return new List<string>
+            {
+                result
+            };
+        }
+
+        private static List<string> AppendName(List<string> responses, string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+                responses = responses.Concat(responses.Select(t => t + " " + name)).ToList();
+            return responses;
+        }
+    }
+}
